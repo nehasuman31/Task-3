def099a422bcfda55027b2b4fff110fc1be8935f
nehasuman31 <32327821+nehasuman31@users.noreply.github.com>
2021-05-13 14:38:36 +0530

    updated

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function testVariable() {
+    let strText = document.getElementById("memid").value;
+    let strText1 = document.getElementById("fname").value;
+    let strText2 = document.getElementById("lname").value;
+    let strText3 = document.getElementById("date").value;
+    sessionStorage.setItem("memid", strText);
+    sessionStorage.setItem("fname", strText1);
+    sessionStorage.setItem("lname", strText2);
+    sessionStorage.setItem("date", strText3);
+}
